7e7de169071b522f9b7c777d2c2a18a07f4f8e19
tacostrophe <tacokong@yandex.ru>
2023-08-03 06:39:33 +0300

    first functionality of createTable

--- apps/createTable/createTable.js
+++ apps/createTable/createTable.js
@@ -1,6 +1,26 @@
-function createTable(...args) {
-    console.log('Create new Table');
-    console.log(args);
-  };
-  
-  module.exports = createTable;
+// Создание таблицы с полями представляющими ФИО, дату рождения, пол.
+
+async function createTable(db, ...args) {
+  console.log('Create new Table');
+  console.log(args);
+  try {
+    const result = await db.query(
+      `CREATE TABLE profiles (
+          pk SERIAL PRIMARY KEY,
+          name VARCHAR (128) NOT NULL,
+          date_of_birth DATE NOT NULL,
+          gender CHAR(1) CHECK(gender in ('F','M')) NOT NULL
+      );`
+    )
+    console.log('Created');
+  } catch (error) {
+    if (error.code === '42P07') { //42P07 - код ошибки duplicate_table
+      console.error('Table already exists');
+    } else {
+      throw error;
+    }
+  }
+
+};
+
+module.exports = createTable;
