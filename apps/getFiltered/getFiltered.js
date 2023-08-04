9f488c5688f179e6df3e3a38ef9f2d7388637e92
Dmitriy <TacoKong@yandex.ru>
2023-08-04 09:18:55 +0300

    added query and timer

--- apps/getFiltered/getFiltered.js
+++ apps/getFiltered/getFiltered.js
@@ -1,6 +1,14 @@
-function getFiltered(...args) {
-    console.log('Get all males with surname, that starts with "F"');
-    console.log(args);
+async function getFiltered(db, ...args) {
+  const timeStart = performance.now();
+  const profilesFiltered = await db.query(
+    'SELECT * FROM profiles WHERE gender=$1 AND name ILIKE $2',
+    ['M', 'f%'],
+  );
+  const timeEnd = performance.now();
+  return {
+    rows: profilesFiltered.rows,
+    execution_time: `${timeEnd - timeStart} ms`,
   };
+};
   
   module.exports = getFiltered;
