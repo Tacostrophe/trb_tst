62cdcd4b4a2d7a9b65fce4422c6244507e4edeff
Dmitriy <TacoKong@yandex.ru>
2023-08-04 09:21:03 +0300

    little fixes

--- apps/createRecord/createRecord.js
+++ apps/createRecord/createRecord.js
@@ -4,13 +4,13 @@ async function createRecord(db, name, dateOfBirth, gender, ...args) {
   if (!(name && dateOfBirth && gender)) {
     console.error(
       'Expected 4 arguments: number of method, name, date_of_birth, gender(M/F)\n'
-      + `for example: node myApp.js Bob 1990-01-01 M`
+      + 'for example: node myApp.js Bob 1990-01-01 M'
     );
     process.exit(1);
   }
   try {
     const newRecord = await db.query(
-      `INSERT INTO profiles (name, date_of_birth, gender) VALUES ($1, $2, $3) RETURNING *;`,
+      'INSERT INTO profiles (name, date_of_birth, gender) VALUES ($1, $2, $3) RETURNING *;',
       [name, dateOfBirth, gender.toUpperCase()],
     );
     return newRecord.rows;
