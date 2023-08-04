ed40f3c3d671ffe7d2cc24d6b268e4e454a355f0
tacostrophe <tacokong@yandex.ru>
2023-08-04 06:58:05 +0300

    added query and catch of duplicate_table error

--- apps/createTable/createTable.js
+++ apps/createTable/createTable.js
@@ -1,21 +1,19 @@
 // Создание таблицы с полями представляющими ФИО, дату рождения, пол.
 
 async function createTable(db, ...args) {
-  console.log('Create new Table');
-  console.log(args);
   try {
-    const result = await db.query(
+    await db.query(
       `CREATE TABLE profiles (
           pk SERIAL PRIMARY KEY,
           name VARCHAR (128) NOT NULL,
           date_of_birth DATE NOT NULL,
           gender CHAR(1) CHECK(gender in ('F','M')) NOT NULL
       );`
-    )
-    console.log('Created');
+    );
   } catch (error) {
     if (error.code === '42P07') { //42P07 - код ошибки duplicate_table
       console.error('Table already exists');
+      process.exit(1);
     } else {
       throw error;
     }
