45318da9c9701318f73f0d83fa5f257fe5d81390
tacostrophe <tacokong@yandex.ru>
2023-08-04 07:01:06 +0300

    added query and catch

--- apps/getAllUnique/getAllUnique.js
+++ apps/getAllUnique/getAllUnique.js
@@ -1,6 +1,24 @@
-function getAllUnique(...args) {
-    console.log('Get all rows with unique "ФИО" + date');
-    console.log(args);
-  };
-  
-  module.exports = getAllUnique;
+// Вывод всех строк с уникальным значением ФИО+дата, отсортированным по ФИО,
+// вывести ФИО, Дату рождения, пол, кол-во полных лет.
+
+async function getAllUnique(db, ...args) {
+  try {
+    const result = await db.query(
+      `SELECT profiles.name, profiles.date_of_birth, profiles.gender,
+        date_part('year', AGE(profiles.date_of_birth)) as age
+      FROM profiles
+      RIGHT JOIN (
+        SELECT name, date_of_birth FROM profiles
+        GROUP BY name, date_of_birth
+        HAVING COUNT(pk)=1
+      ) as filtered
+      ON filtered.name = profiles.name AND filtered.date_of_birth = profiles.date_of_birth
+      ORDER BY name;`
+    )
+    return result.rows;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = getAllUnique;
