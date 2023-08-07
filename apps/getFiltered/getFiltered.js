01673e7669780555ae6ae4325f461f480ae1a0f9
tacostrophe <tacokong@yandex.ru>
2023-08-07 08:24:20 +0300

    added fast filter

--- apps/getFiltered/getFiltered.js
+++ apps/getFiltered/getFiltered.js
@@ -1,4 +1,4 @@
-async function getFiltered(db, ...args) {
+async function filter(db, ...args) {
   const timeStart = performance.now();
   const profilesFiltered = await db.query(
     'SELECT * FROM profiles WHERE gender=$1 AND name ILIKE $2',
@@ -10,5 +10,28 @@ async function getFiltered(db, ...args) {
     execution_time: `${timeEnd - timeStart} ms`,
   };
 };
+
+async function getFiltered(db, ...args) {
+  // Без индекса фильтрация происходит обычным перебором
+  await db.query(
+    'DROP INDEX IF EXISTS idx_name_gender;'
+  );
+  let {execution_time} = await filter(db, ...args);
+  // Индекс указывает, где в таблице находятся подходящие под условие (которое было указано и в индексе)
+  await db.query(
+    `CREATE INDEX IF NOT EXISTS idx_name_gender ON profiles
+    (
+      name,
+      gender
+    )
+    WHERE name ILIKE 'f%' AND gender='M';`
+  );
+  const result = await filter(db, ...args);
+  return {
+    rows: result.rows,
+    execution_time_slow: execution_time,
+    execution_time_fast: result.execution_time,
+  };
+}
   
   module.exports = getFiltered;
